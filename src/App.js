9cc536a32ece5ba0f1a16edaec6847705578b683
Alfonso <61892420+ShrikeFound@users.noreply.github.com>
2021-03-25 17:43:25 -0700

    added dependancies to json (for real)
    whoops..

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 //React Suite
 import { Route, Switch } from 'react-router';
 import 'rsuite/dist/styles/rsuite-default.css';
+import PrivateRoute from './components/PrivateRoute';
+import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import './styles/main.scss';
 
@@ -9,9 +11,12 @@ import './styles/main.scss';
 function App() {
   return (
     <Switch>
-      <Route>
+      <Route path="/signin">
         <SignIn/>
       </Route>
+      <PrivateRoute path="/">
+        <Home/>
+      </PrivateRoute>
     </Switch>
   );
 }
