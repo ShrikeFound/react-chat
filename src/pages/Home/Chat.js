455a8361d3d06d56573c2223a2e7d929e7a165eb
Alfonso <61892420+ShrikeFound@users.noreply.github.com>
2021-03-27 19:55:59 -0700

    installed context-selector(why in description)
    splitting a context into multiple prevents components that rely on only a single part of that context from rerendering every time any part of the context changes. So that's what we're gonna do.;

--- src/pages/Home/Chat.js
+++ src/pages/Home/Chat.js
@@ -1,9 +1,48 @@
 import React from 'react'
+import ChatTop from '../../components/chat-window/top'
+import ChatBottom from '../../components/chat-window/bottom'
+import Messages from '../../components/chat-window/messages'
+import { useParams } from 'react-router'
+import { useRoom } from '../../context/room.context'
+import { Loader } from 'rsuite'
+
 
 const Chat = () => {
+
+  const { firesideId } = useParams();
+
+  const rooms = useRoom();
+
+  if (!rooms) {
+    return <Loader center vertical size="md" content="Loading" speed="slow" />
+  }
+
+
+  const currentRoom = rooms.find(room => {
+    console.log(room.id,firesideId,room.id === firesideId)
+    return  room.id === firesideId
+  });
+  console.log(rooms)
+  if (!currentRoom) {
+    return <h6 className="mt-c center">Fireside Not Found</h6>
+  }
+
   return (
     <div>
-      hello
+      <div>
+        <ChatTop/>
+      </div>
+
+      <div>
+        <Messages/>
+      </div>
+
+
+      <div>
+        <ChatBottom/>
+      </div>
+
+
     </div>
   )
 }
